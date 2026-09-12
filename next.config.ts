ced9250ad11cc3ee24cb8e362d020c112d777839
Alvin Binoy <alvinbinoy2006@gmail.com>
2026-09-12 14:53:39 +0530

    configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // If your repository is not the user root (e.g., username.github.io/aquaxchange_3d),
+  // you must set the basePath and assetPrefix so it finds your files.
+  basePath: "/aquaxchange_3d",
+  assetPrefix: "/aquaxchange_3d/",
 };
 
 export default nextConfig;
